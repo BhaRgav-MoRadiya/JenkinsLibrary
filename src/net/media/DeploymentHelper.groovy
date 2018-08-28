ed17f8e265a2a92e2d346b4ec78ef79ded7b6f21
Vishal Garg <garg.v@media.net>
2018-08-28 18:25:43 +0530

    Initial commit

---
+++ src/net/media/DeploymentHelper.groovy
@@ -0,0 +1,81 @@
+
+/*
+  Zip the given files.
+*/
+def zipd(String inclusion, String fileName, String targetPath){
+  //fileName is the jar to be deployed.
+  //inclusion are the files provided by user to be zipped using includeInZip
+  zipper=  """
+      cd ${targetPath}
+      zip -r ${fileName}.zip ${fileName} ${inclusion}
+  """
+  zipped = sh( script: zipper, returnStatus: true)
+  if( zipped == 0)
+    print("[DEPLOY LIB] zip success.")
+  else
+    abortBuild("[DEPLOY LIB] zip failed for ${filename} and ${inclusion}")
+}
+
+
+/*
+  Initiate rsync of zip with destination IP addresses.
+*/
+def initMonolithDelivery(Map properties, String fileName){
+  for(ip in properties['ip']){
+    rsync = "rsync -e 'ssh -o StrictHostKeyChecking=no' -avrzP ${targetPath}/${fileName} ${properties['user']}@${ip}:${properties['destinationPath']}"
+    output = sh (script: rsync, returnStdout: true)
+    println(output)
+  }
+}
+
+
+
+/*
+  Abort builds with custom message.
+  This aborts the build not fail it.
+*/
+def abortBuild(String msg){
+    //currentBuild variable is available by default.
+    currentBuild.result = 'ABORTED'
+    error(msg)
+}
+
+
+/*
+  Enforces namespaces for dockerized builds.
+*/
+def enforceNamespace(String appName){
+  splitter = appName.split("/")
+  if( splitter.length() !=4 )
+    abortBuild("Namespace enforcement failed. Expected format '{TeamName}/{Environment}/{ProjectName}/{ApplicationName}'")
+
+  print("Enforcing namespace.")
+}
+
+
+/*
+  Check if all required params are provided
+  for normal functioning of script.
+*/
+def propertiesVerifier(String[] required, Map properties, Boolean dockerize){
+
+  def required = ["user", "ip", "destinationPath", "type"] //required fields for program to work.
+  def dockerizeRequired = ["appName", "tag"] //fields required for dockerizing
+
+  if(dockerize==true){
+    for( String param in dockerizeRequired ){
+      if(!properties.containsKey(param))
+        abortBuild("Missing required key: ${param}")
+    }
+  }
+
+  for( String param in required ){
+      if(!properties.containsKey(param))
+        abortBuild("Missing required key: ${param}")
+  }
+
+}
+
+
+//required for some reason
+return this
---
+++ vars/deploy.groovy
@@ -0,0 +1,76 @@
+/*
+	Share library to deploy jar or war files.
+	Supports zipping, and dockerizing.
+*/
+
+/*
+			def params = [:]
+			params['user'] = 'root'
+			params['ip'] = ['172.17.0.3']
+			params['zip'] = true //or false
+			params['includeInZip'] = "lib/"
+			params['destinationPath'] = '/root/hello'
+			params['dockerize'] = false
+			params['type'] = 'jar'
+			params['targetPath'] = "single-module/target"
+			params['appName']= "autoopts/crawler/proxytunnel"
+			params['tag'] = "prod"
+			params['parametrizedMesos'] = {
+				"id": "autoopts3/crawler/proxytunnel",
+				"instances": 2
+			}
+*/
+
+
+import net.media.*
+
+def call(Map properties){
+	def targetPath = ""
+	def includeInZip = ""
+
+	if(properties.containsKey('dockerize') && properties['dockerize']){
+		enforceNamespace(properties['appName']) 
+	}
+
+	if( properties.containsKey("targetPath"))
+		targetPath = "${WORKSPACE}/" + properties['targetPath']
+	else
+		targetPath = "${WORKSPACE}/target"
+
+	def getFilePath = "find ${targetPath}/ -maxdepth 1 -name '*.${properties['type']}' 2>/dev/null"
+
+
+	try {
+			filePath = sh (script: getFilePath, returnStdout: true).trim()
+			fileName = filePath.split("/")[-1]
+		} catch(Exception e){
+			abortBuild("[DEPLOY LIB] PATH MOST LIKELY WRONG FOR TARGET DIRECTORY. SCRIPT LOOKING INTO  ${targetPath}. Use relative paths for targetPath param.")
+		}
+
+
+	if( properties['zip'] == true ){
+			if( properties.containsKey("includeInZip"))
+				includeInZip = properties['includeInZip']
+			zipd("${includeInZip}", fileName, targetPath)
+	}
+
+
+	if(properties['dockerize'] == true){	
+		docker = """
+			cd ${targetPath}
+			docker build -t r.reports.mn/${properties['appName']}:${tag} -f Dockerfile ${WORKSPACE}
+			docker push r.reports.mn/${properties['appName']}:${tag}
+		"""
+		buildStatus = sh(script: docker, returnStatus: true)
+		if(buildStatus==0)
+			print("Build push complete.")
+		else
+			print("Build failure.")
+  }
+
+	else {
+		initMonolithDelivery(properties, fileName ,targetPath)
+	}
+
+}
+
