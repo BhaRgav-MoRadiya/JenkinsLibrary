066d1f9fae84872648ec72a92dac575b5d3fa819
Vishal Garg <garg.v@media.net>
2018-08-31 15:44:47 +0530

    automated comment

--- vars/deploy.groovy
+++ vars/deploy.groovy
@@ -63,10 +63,6 @@ def call(Map properties){
 		stage('Building docker image'){
 			app = docker.build("${properties['appName']}")
 		}
-		stage('Pushing to reports.mn'){
-			docker.withRegistry('http://r.reports.mn')	{
-				app.push('latest')
-			}
 		}
   }
 
