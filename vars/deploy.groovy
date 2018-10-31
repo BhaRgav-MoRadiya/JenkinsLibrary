8d95e296eb9b4b5118370101a28166384579649a
Vishal <garg.v@media.net>
2018-10-31 15:41:17 +0530

    Add support for custom Build options

--- vars/deploy.groovy
+++ vars/deploy.groovy
@@ -20,6 +20,7 @@
 			params['dockerfile'] = "Dockerfile.en"
 			params['onlyBuild'] = true
 			params['marathonEndpoint'] = "http://dcos-master-1.og.reports.mn:8080/v2/apps/"
+			params['customArgs']  = "--build-arg deployment_env=prod"
 */
 
 
@@ -62,8 +63,18 @@ def call(Map properties){
 
 	if(properties['dockerize'] == true){
 		stage('Building docker image'){
-			if(properties.containsKey("dockerfile"))
-				app = docker.build("${properties['appName']}", "-f ${properties['dockerfile']} .")
+			extraParams = ""
+			extraBoolean = false
+			if(properties.containsKey("customArgs")){
+				extraParams = properties['customArgs']
+				extraBoolean = true
+			}
+			if(properties.containsKey("dockerfile")){
+				extraParams += " -f ${properties['dockerfile']} ."
+				extraBoolean = true
+			}
+			if(extraBoolean)
+				app = docker.build("${properties['appName']}", extraParams)
 			else
 				app = docker.build("${properties['appName']}")
 		}
