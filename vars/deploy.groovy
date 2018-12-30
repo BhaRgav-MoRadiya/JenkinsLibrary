2fc33c991d3c4854bbf9e14139012d76610361e0
BhaRgav MoRadiya <bhargav.m@media.net>
2018-12-30 13:32:31 +0530

    removing dockerfile

--- vars/deploy.groovy
+++ vars/deploy.groovy
@@ -31,7 +31,7 @@ def call(Map constants){
 	//def aws_docker_command = """docker run --rm -t \$(tty &>/dev/null && echo "-i") -e "AWS_ACCESS_KEY_ID=\${AWS_ACCESS_KEY_ID}" -e "AWS_SECRET_ACCESS_KEY=\${AWS_SECRET_ACCESS_KEY}" -e "AWS_DEFAULT_REGION=${REGION}" mesosphere/aws-cli """
 	def customImage = ''
 	constants['msgForFlock']="Starting build..!!\n"
-	constants['dockerfilePath']="subdir/Dockerfile"
+	//constants['dockerfilePath']="subdir/Dockerfile"
 	helper = new DeploymentHelper()
 	helper.setDefaults(constants)
 	helper.prerequisite(constants)
